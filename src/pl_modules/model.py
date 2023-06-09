bf6466eed624fd4c799833b87f929170850941e5
naamirani <43768250+naamirani@users.noreply.github.com>
2023-06-09 13:37:04 -0700

    Update model.py

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -151,7 +151,8 @@ class MyModel(pl.LightningModule):
             "val_loss": out["loss"].mean(),
         }
 
-    def validation_epoch_end(self, outputs: List[Any]) -> None:
+    #def on_validation_epoch_end(self, outputs: List[Any]) -> None:
+    def on_validation_epoch_end(self, *_):
         if hasattr(self.net, "encoder"):
             layer = self.net.encoder.layer4
         else:
